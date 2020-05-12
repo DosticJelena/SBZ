06b1ffb5e779e7051388cf6415c84cf5127a2690
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-12 22:12:18 +0200

    created responsive ingredient dropdown
    - storing choosen ingredients in "choosenIngredients" list
    - fixed style

--- sbz-frontend/src/components/FindRecipe/FindRecipe.js
+++ sbz-frontend/src/components/FindRecipe/FindRecipe.js
@@ -2,22 +2,29 @@ import React from 'react';
 import logo from '../../assets/hop.png';
 
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class FindRecipe extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            ingredients: []
+            ingredients: [],
+            choosenIngredients: []
         }
+    }
+
+    chooseIngredient = (e) => {
+        console.log(e.target.value);
+        this.setState({choosenIngredients: [...this.state.choosenIngredients, e.target.value]})
     }
 
     componentDidMount() {
         axios.get("http://localhost:8080/ingredients")
-        .then((response) => {
-            this.setState({ingredients: response.data});
-        })
-        .catch(error => console.log(error))
+            .then((response) => {
+                this.setState({ ingredients: response.data });
+            })
+            .catch(error => console.log(error))
     }
 
     render() {
@@ -26,21 +33,41 @@ class FindRecipe extends React.Component {
                 <nav>
                     <img src={logo} alt="logo-img" width="64" height="64" />
                     <div className="home-logo">
-                        <h3>The Recipe Blend</h3>
+                        <Link to="/"><h3>The Recipe Blend</h3></Link>
                         <p>Find Recipe</p>
                     </div>
                 </nav>
                 <div className="filter row">
-                    <div className="step-1 col-4">
+                    <div className="step-1 col-md-4 col-12">
                         <h4>Step 1: Choose ingredients</h4>
                         <hr />
-                        {this.state.ingredients.map((ing)=> <p key={ing.name}>{ing.name}</p>)}
+                        <div className="dropdown">
+                            <button className="btn btn-secondary dropdown-toggle ing-dropdown" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Ingredient List
+                            </button>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                {this.state.ingredients.map((ing) => {
+                                    return (
+                                        <div key={ing.name} className="checkbox">
+                                            <label>
+                                                <input name="ingredient" value={ing.name} onClick={this.chooseIngredient} type="checkbox" />{ing.name}
+                                            </label>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                        <hr />
+                        <h5>Choosen ingredients:</h5>
+                        <div className="choosen-ingredients">
+                                {this.state.choosenIngredients.map((ing) => <h6 key={ing}>- {ing}</h6>)}
+                        </div>
                     </div>
-                    <div className="step-2 col-4">
+                    <div className="step-2 col-md-4 col-12">
                         <h4>Step 2: Additional filters</h4>
                         <hr />
                     </div>
-                    <div className="step-3 col-4">
+                    <div className="step-3 col-md-4 col-12">
                         <h4>Step 3: Find Recipe!</h4>
                         <hr />
                     </div>
