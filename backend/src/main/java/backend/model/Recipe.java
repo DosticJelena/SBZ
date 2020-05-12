9885df0640585f66b28e3602dd5d0b41fe7b7c8a
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-12 12:17:28 +0200

    implemented Recipe class

--- backend/src/main/java/backend/model/Recipe.java
+++ backend/src/main/java/backend/model/Recipe.java
@@ -1,4 +1,44 @@
 package backend.model;
 
 public class Recipe {
+
+    private String name;
+    private Set<Ingredient> ingredients;
+    private Macronutrients macros;
+
+    public Recipe() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Macronutrients getMacros() {
+        return macros;
+    }
+
+    public void setMacros(Macronutrients macros) {
+        this.macros = macros;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "name='" + name + '\'' +
+                ", ingredients=" + ingredients +
+                ", macros=" + macros +
+                '}';
+    }
 }
