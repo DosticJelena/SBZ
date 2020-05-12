b9df23215976a40a8d6f027a02196645a652e673
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-12 12:16:17 +0200

    implemented Macronutrients class

--- backend/src/main/java/backend/model/Macronutrients.java
+++ backend/src/main/java/backend/model/Macronutrients.java
@@ -2,6 +2,53 @@ package backend.model;
 
 public class Macronutrients {
 
-    
+    private double protein;
+    private double carbs;
+    private double fat;
+    private double calories;
 
+    public Macronutrients() {
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(double carbs) {
+        this.carbs = carbs;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+
+    @Override
+    public String toString() {
+        return "Macronutrients{" +
+                "protein=" + protein +
+                ", carbs=" + carbs +
+                ", fat=" + fat +
+                ", calories=" + calories +
+                '}';
+    }
 }
