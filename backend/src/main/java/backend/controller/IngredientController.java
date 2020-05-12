9873a9bee8993099a54aabc3c16a7602f2e4149a
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-12 18:49:18 +0200

    changed paths

--- backend/src/main/java/backend/controller/IngredientController.java
+++ backend/src/main/java/backend/controller/IngredientController.java
@@ -1,15 +1,23 @@
 package backend.controller;
 
+import backend.model.Ingredient;
 import backend.service.serviceInterface.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("ingredient")
+@RequestMapping("ingredients")
 public class IngredientController {
 
     @Autowired
     private IngredientService ingredientService;
 
+    @GetMapping(value="")
+    public List<Ingredient> getAll(){
+        return ingredientService.getAll();
+    }
 }
--- backend/src/main/java/backend/controller/RecipeController.java
+++ backend/src/main/java/backend/controller/RecipeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("recipe")
+@RequestMapping("recipes")
 public class RecipeController {
 
     @Autowired
