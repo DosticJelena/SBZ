6a9ce0224a7e6fd1143cce8e32dae157321d38ae
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-12 15:11:31 +0200

    added annotations

--- backend/src/main/java/backend/service/MacronutrientsServiceImpl.java
+++ backend/src/main/java/backend/service/MacronutrientsServiceImpl.java
@@ -1,6 +1,8 @@
 package backend.service;
 
 import backend.service.serviceInterface.MacronutrientsService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MacronutrientsServiceImpl implements MacronutrientsService {
 }
--- backend/src/main/java/backend/service/RecipeServiceImpl.java
+++ backend/src/main/java/backend/service/RecipeServiceImpl.java
@@ -1,6 +1,8 @@
 package backend.service;
 
 import backend.service.serviceInterface.RecipeService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RecipeServiceImpl implements RecipeService {
 }
