9f1057a9818cad89763b1a95d5fef45c03202e3f
DosticJelena <jelenadostic2@gmail.com>
2020-05-18 14:58:13 +0200

    added method for listing all recipes

--- backend/src/main/java/backend/controller/RecipeController.java
+++ backend/src/main/java/backend/controller/RecipeController.java
@@ -1,9 +1,13 @@
 package backend.controller;
 
+import backend.model.Recipe;
 import backend.service.serviceInterface.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("recipes")
@@ -11,5 +15,10 @@ public class RecipeController {
 
     @Autowired
     private RecipeService recipeService;
+
+    @GetMapping(value="/")
+    public List<Recipe> getAllRecipes(){
+        return recipeService.findAll();
+    }
 
 }
--- backend/src/main/java/backend/service/RecipeServiceImpl.java
+++ backend/src/main/java/backend/service/RecipeServiceImpl.java
@@ -1,8 +1,11 @@
 package backend.service;
 
+import backend.kie.util.KnowledgeSessionHelper;
 import backend.model.Recipe;
 import backend.repository.RecipeRepository;
 import backend.service.serviceInterface.RecipeService;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +29,18 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public List<Recipe> findAll(){
+        KieContainer kc = KnowledgeSessionHelper.createRuleBase();
+        KieSession kSession = KnowledgeSessionHelper.getStatefulKnowledgeSession(kc, "meals-rules");
+
+        List<Recipe> recipes = recipeRepository.findAll();
+
+        for (Recipe r : recipes){
+            kSession.insert(r);
+        }
+
+        kSession.getAgenda().getAgendaGroup("recipeSearchResults").setFocus();
+        kSession.fireAllRules();
+
         return recipeRepository.findAll();
     }
 
