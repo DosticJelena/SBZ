89d9ed05a2d6818e458775e8c63780ab455bf53c
DosticJelena <jelenadostic2@gmail.com>
2020-05-30 01:32:10 +0200

    added location and allergen options

--- sbz-frontend/src/components/FindRecipe/FindRecipe.js
+++ sbz-frontend/src/components/FindRecipe/FindRecipe.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from '../../assets/hop.png';
+import locationLogo from '../../assets/pin.png';
 
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 import SearchResults from './SearchResults/SearchResults';
+import SmallModal from '../UI/Modal/SmallModal';
 
 class FindRecipe extends React.Component {
 
@@ -12,15 +14,17 @@ class FindRecipe extends React.Component {
         super(props);
         this.state = {
             ingredients: [],
-            choosenIngredients: [],
+            locations: [],
             allergens: [],
+            choosenIngredients: [],
+            choosenAllergens: [],
             location: '',
             validRec: [],
             continentRec: [],
             worldRec: [],
             semiRec: [],
-            ref: null,
-            showSearchResults: false
+            showSearchResults: false,
+            smallModalVisible: false
         }
     }
 
@@ -35,7 +39,20 @@ class FindRecipe extends React.Component {
         }
     }
 
+    chooseLocation = (e) => {
+        this.setState({ location: e.target.value }, console.log(this.state.location));
+    }
+
+    modalHandler = () => {
+        this.setState({ smallModalVisible: true });
+    }
+
+    modalClosedHandler = () => {
+        this.setState({ smallModalVisible: false });
+    }
+
     searchRecipes = () => {
+        this.modalHandler();
         axios.post("http://localhost:8080/recipes/search", {
             ingredients: this.state.choosenIngredients,
             allergens: this.state.allergens,
@@ -53,10 +70,8 @@ class FindRecipe extends React.Component {
                 })
                 this.scrollToMyRef();
             })
-            .catch(error => console.log(error))
+            .catch(error => {console.log(error); this.modalClosedHandler()})
     }
-
-    scrollToMyRef = () => window.scrollTo(0, this.state.ref.offsetTop)
 
     componentDidMount() {
         axios.get("http://localhost:8080/ingredients")
@@ -64,11 +79,66 @@ class FindRecipe extends React.Component {
                 this.setState({ ingredients: response.data });
             })
             .catch(error => console.log(error))
+
+        axios.get("http://localhost:8080/locations")
+            .then((response) => {
+                this.setState({ locations: response.data });
+            })
+            .catch(error => console.log(error))
+
+        axios.get("http://localhost:8080/allergens")
+            .then((response) => {
+                this.setState({ allergens: response.data });
+            })
+            .catch(error => console.log(error))
     }
 
     render() {
+
+        let allergenContent;
+        this.state.allergens.length === 0 ?
+            allergenContent = <label>There is no allergens to list.</label> :
+            allergenContent = this.state.allergens.map((alg) => {
+                return (
+                    <div key={alg.name} className="checkbox">
+                        <label>
+                            <input name="location" value={alg.name} onClick={this.chooseLocation} type="checkbox" />
+                            {alg.name}
+                        </label>
+                    </div>
+                )
+            })
+
+        let ingredientContent;
+        this.state.ingredients.length === 0 ?
+            ingredientContent = <label>Ingredients loading...</label> :
+            ingredientContent = this.state.ingredients.map((ing) => {
+                return (
+                    <div key={ing.name} className="checkbox">
+                        <label>
+                            <input name="ingredient" value={ing.name} onClick={this.chooseIngredient} type="checkbox" />{ing.name}
+                        </label>
+                    </div>
+                )
+            })
+
+        let locationContent;
+        this.state.locations.length === 0 ?
+            locationContent = <label>Locations loading...</label> :
+            locationContent = this.state.locations.map((location) => {
+                return (
+                    <div key={location.name} className="checkbox">
+                        <label>
+                            <input name="location" checked={location.name === this.state.location} value={location.name} onClick={this.chooseLocation} type="checkbox" />
+                            {location.name} | {location.continent}
+                        </label>
+                    </div>
+                )
+            })
+
         return (
             <div className="FindRecipe">
+                <SmallModal show={this.state.smallModalVisible}><h3>Please Wait...</h3></SmallModal>
                 <nav>
                     <div className="home-logo">
                         <Link to="/"><h3>The Recipe Blender <img src={logo} alt="logo-img" width="35" height="35" /></h3></Link>
@@ -84,15 +154,7 @@ class FindRecipe extends React.Component {
                                 Ingredient List
                             </button>
                             <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-                                {this.state.ingredients.map((ing) => {
-                                    return (
-                                        <div key={ing.name} className="checkbox">
-                                            <label>
-                                                <input name="ingredient" value={ing.name} onClick={this.chooseIngredient} type="checkbox" />{ing.name}
-                                            </label>
-                                        </div>
-                                    )
-                                })}
+                                {ingredientContent}
                             </div>
                         </div>
                         <hr />
@@ -104,6 +166,28 @@ class FindRecipe extends React.Component {
                     <div className="step-2 col-md-4 col-12">
                         <h4>Step 2: Additional filters</h4>
                         <hr />
+                        <h5>Location</h5>
+                        <div className="dropdown">
+                            <button className="btn btn-secondary dropdown-toggle ing-dropdown" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Choose Location
+                            </button>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                {locationContent}
+                            </div>
+                        </div>
+                        <br />
+                        <h5><img src={locationLogo} alt="location" height="32" width="32" /> {this.state.location}</h5>
+                        <hr />
+                        <br />
+                        <h5>Allergens</h5>
+                        <div className="dropdown">
+                            <button className="btn btn-secondary dropdown-toggle ing-dropdown" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Allergen List
+                            </button>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                {allergenContent}
+                            </div>
+                        </div>
                     </div>
                     <div className="step-3 col-md-4 col-12">
                         <h4>Step 3: Find Recipe!</h4>
@@ -113,12 +197,15 @@ class FindRecipe extends React.Component {
                         </button>
                     </div>
                 </div>
-                <div className="row" ref={(ref) => this.myRef = ref}>
+                <div className="row">
                     {this.state.showSearchResults ? <SearchResults
                         validRec={this.state.validRec}
                         continentRec={this.state.continentRec}
                         worldRec={this.state.worldRec}
                         semiRec={this.state.semiRec}
+                        location={this.state.location}
+                        ingr={this.state.choosenIngredients}
+                        aler={this.state.choosenAllergens}
                     /> : null}
                 </div>
             </div>
