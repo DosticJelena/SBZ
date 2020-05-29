82c2802424306afe29808ec73588fdf6903c8fe3
DosticJelena <jelenadostic2@gmail.com>
2020-05-30 01:36:02 +0200

    fixed empty path

--- backend/src/main/java/backend/controller/AllergenController.java
+++ backend/src/main/java/backend/controller/AllergenController.java
@@ -1,9 +1,14 @@
 package backend.controller;
 
+import backend.model.Allergen;
+import backend.model.Recipe;
 import backend.service.serviceInterface.AllergenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("allergens")
@@ -11,5 +16,10 @@ public class AllergenController {
 
     @Autowired
     private AllergenService allergenService;
+
+    @GetMapping(value="")
+    public List<Allergen> getAllRecipes(){
+        return allergenService.findAll();
+    }
 
 }
--- backend/src/main/java/backend/controller/LocationController.java
+++ backend/src/main/java/backend/controller/LocationController.java
@@ -1,9 +1,14 @@
 package backend.controller;
 
+import backend.model.Location;
+import backend.model.Recipe;
 import backend.service.serviceInterface.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("locations")
@@ -11,5 +16,10 @@ public class LocationController {
 
     @Autowired
     private LocationService locationService;
+
+    @GetMapping(value="")
+    public List<Location> getAll(){
+        return locationService.findAll();
+    }
 
 }
